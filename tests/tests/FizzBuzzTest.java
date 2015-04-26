525689691c1460ef645a448731683d74ee750ada
Jeramie <jeramie.risinger@gmail.com>
2015-04-26 16:10:46 -0700

    require fizz for multiples of 3 failing test.

--- tests/tests/FizzBuzzTest.java
+++ tests/tests/FizzBuzzTest.java
@@ -9,9 +9,9 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    public void buzzPrintsDelimitedCountFromOneToFifteen() throws Exception {
+    public void buzzHandlesMultiplesOfThree() throws Exception {
         FizzBuzz testObj = new FizzBuzz();
-        String expected = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15";
+        String expected = "1,2,Fizz,4,5,Fizz,7,8,Fiz,10,11,Fizz,13,14,Fizz";
 
         String actual = testObj.buzz();
 
