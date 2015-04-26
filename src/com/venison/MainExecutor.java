d7c1caedacf63e38b4805110cac35a075f44d23e
Jeramie <jeramie.risinger@gmail.com>
2015-04-26 16:54:43 -0700

    Running it now actually prints out the values.

--- src/com/venison/MainExecutor.java
+++ src/com/venison/MainExecutor.java
@@ -3,12 +3,14 @@ package com.venison;
 public class MainExecutor {
 
     private WriteStuff writer;
+    private FizzBuzz fizz;
 
     public MainExecutor(WriteStuff writer, FizzBuzz fizzBuzz){
         this.writer = writer;
+        this.fizz = fizzBuzz;
     }
     public void execute() {
-        writer.Write("");
+        writer.Write(fizz.buzz());
     }
 }
 
