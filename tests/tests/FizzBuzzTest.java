d27ad6d460b7a654a444b6919feb89dcc7af28da
Jeramie <jeramie.risinger@gmail.com>
2015-04-26 16:13:01 -0700

    Requiring buzz for multiples of 5

--- tests/tests/FizzBuzzTest.java
+++ tests/tests/FizzBuzzTest.java
@@ -9,9 +9,9 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    public void buzzHandlesMultiplesOfThree() throws Exception {
+    public void buzzHandlesMultiplesOfThreeAndFive() throws Exception {
         FizzBuzz testObj = new FizzBuzz();
-        String expected = "1,2,Fizz,4,5,Fizz,7,8,Fiz,10,11,Fizz,13,14,Fizz";
+        String expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fiz,Buzz,11,Fizz,13,14,FizzBuzz";
 
         String actual = testObj.buzz();
 
