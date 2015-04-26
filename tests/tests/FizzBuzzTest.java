1efa99f1e7ff676a853c524bf580135eb4005073
Jeramie <jeramie.risinger@gmail.com>
2015-04-26 16:08:06 -0700

    Next fail test.

--- tests/tests/FizzBuzzTest.java
+++ tests/tests/FizzBuzzTest.java
@@ -9,9 +9,9 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    public void buzzReturnsString() throws Exception {
+    public void buzzPrintsDelimitedCountFromOneToFifteen() throws Exception {
         FizzBuzz testObj = new FizzBuzz();
-        String expected = "";
+        String expected = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15";
 
         String actual = testObj.buzz();
 
