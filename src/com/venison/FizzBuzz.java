6304eea519328ad57960d4e091d7470bc8f0da35
Jeramie <jeramie.risinger@gmail.com>
2015-04-26 16:33:46 -0700

    Small refactor step.  Added extra test to verify one beyond currently tested bounds.  Small refactorings in code to remove magic strings.

--- src/com/venison/FizzBuzz.java
+++ src/com/venison/FizzBuzz.java
@@ -3,32 +3,31 @@ package com.venison;
 public class FizzBuzz {
     private int upper;
     private int lower;
+    private final String ModThreeToken = "Fizz";
+    private final String ModFiveToken = "Buzz";
+    private final String SequenceDelimiter = ",";
 
     public FizzBuzz(int lower, int upper) {
         this.lower = lower;
         this.upper = upper;
     }
 
-    public FizzBuzz() {
-
-    }
-
     public String buzz(){
         StringBuilder builder = new StringBuilder();
         for(int i=lower; i<=upper;i++){
             if(i>lower){
-                builder.append(",");
+                builder.append(SequenceDelimiter);
             }
             if((i % 3) == 0 && (i % 5 )== 0){
-                builder.append("FizzBuzz");
+                builder.append(ModThreeToken);
+                builder.append(ModFiveToken);
             }else if((i % 3) == 0){
-                builder.append("Fizz");
+                builder.append(ModThreeToken);
             }else if((i % 5) == 0){
-                builder.append("Buzz");
+                builder.append(ModFiveToken);
             }else{
                 builder.append(i);
             }
-
         }
         return builder.toString();
     }
--- tests/tests/FizzBuzzTest.java
+++ tests/tests/FizzBuzzTest.java
@@ -10,7 +10,9 @@ public class FizzBuzzTest {
 
     @Test
     public void buzzHandlesMultiplesOfThreeAndFive() throws Exception {
-        FizzBuzz testObj = new FizzBuzz(1, 15);
+        int lower = 1;
+        int upper = 15;
+        FizzBuzz testObj = BuildTestObject(upper, lower);
         String expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz";
 
         String actual = testObj.buzz();
@@ -18,12 +20,28 @@ public class FizzBuzzTest {
         assertEquals(expected, actual);
     }
 
+    private FizzBuzz BuildTestObject(int upper, int lower) {
+        return new FizzBuzz(lower, upper);
+    }
+
     @Test
     public void buzz_givenBounds_printsProperStatement() throws Exception {
         int lower = 5;
         int upper = 15;
-        FizzBuzz testObj = new FizzBuzz(lower, upper);
+        FizzBuzz testObj = BuildTestObject(upper, lower);
         String expected = "Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz";
+
+        String actual = testObj.buzz();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void buzz_givenBoundsAboveLastFizzBuzzPrintout_printsProperStatement() throws Exception {
+        int lower = 5;
+        int upper = 16;
+        FizzBuzz testObj = BuildTestObject(upper, lower);
+        String expected = "Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16";
 
         String actual = testObj.buzz();
 
