6f95782d735e4eda8e456dfa2ec2b0407acef550
Jeramie <jeramie.risinger@gmail.com>
2015-04-26 16:11:27 -0700

    buzz handles multiples of 3 up to 15.

--- src/com/venison/FizzBuzz.java
+++ src/com/venison/FizzBuzz.java
@@ -2,6 +2,6 @@ package com.venison;
 
 public class FizzBuzz {
     public String buzz(){
-        return "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15";
+        return "1,2,Fizz,4,5,Fizz,7,8,Fiz,10,11,Fizz,13,14,Fizz";
     }
 }
